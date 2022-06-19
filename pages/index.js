e907fa2f84b4d5e7a86e644ad608dad69e9cbdd6
KIMSANGYEOB <sykim10070@gmail.com>
2022-06-19 12:51:23 +0900

    dynamic Routes 구성

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,6 @@ import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
-}
-
 export default function Home({ allPostsData }) {
   return (
     <Layout home>
@@ -41,4 +32,13 @@ export default function Home({ allPostsData }) {
       </section>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
